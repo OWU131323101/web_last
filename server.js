78b8890daf9b0c9ce9fbc8c5fcebc5e276eafc27
OkitaAsuka <asu02kkmsn@gmail.com>
2026-01-14 11:49:52 +0900

    Add request logging to debug Cloud Shell 500 error

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ const io = new Server(server);
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+});
 app.use(express.static('public'));
 
 // --- Configuration ---
